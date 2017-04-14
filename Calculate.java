282125cfe5ab87c1b89137a8c762daf269495ebc
LastHero91 <kam.06@mail.ru>
2017-04-15 01:42:46 +0300

    Test file

--- Calculate.java
+++ Calculate.java
@@ -3,8 +3,12 @@ public class Calculate{
 		int first, second, result;
 		first=Integer.valueOf(args[0]);
 		second=Integer.valueOf(args[1]);
-		result=first+second;
+		result=multiplay(first, second);
 		System.out.println("result="+result);
 		System.out.println("Complete!");
+	}
+	
+	private static int multiplay(int x, int y){
+		return x+y;
 	}
 }
