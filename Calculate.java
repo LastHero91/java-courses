823f1d805a160a76ac71f0f0824fb337e9a39ed1
LastHero91 <kam.06@mail.ru>
2017-04-14 23:40:56 +0300

    Test make new branch

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,10 @@
 public class Calculate{
 	public static void main(String args[]){
+		int first, second, result;
+		first=Integer.valueOf(args[0]);
+		second=Integer.valueOf(args[1]);
+		result=first+second;
+		System.out.println("result="+result);
 		System.out.println("Complete!");
 	}
 }
